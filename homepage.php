53e3d351622b4350d22f845f548413ba4e6533ba
Sebastian08889 <sebastian.dahl@edu.huddinge.se>
2026-05-22 07:50:13 +0000

    Började Designa

---
+++ homepage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Min Hemsida</title>
+    <h1>Min Hemsida</h1>
+    <h2>Välkommen till min hemsida!</h2>
+    </head>
+<body>
+<a href="andra.php" class="link1">Klicka här!</a>
+
+<img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="Träd" width="500" height="600" title="Träd">
+
+</body>
+</html>
